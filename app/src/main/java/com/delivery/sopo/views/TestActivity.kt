b0d29f239b0c8b4c7e27427ddf42b745eb85ea3b
Kyunghyun Park <kyunghyun@telcuon.com>
2020-10-04 14:27:15 +0900

    [UPDATE] 1. resource 추가

---
+++ app/src/main/java/com/delivery/sopo/views/TestActivity.kt
@@ -0,0 +1,22 @@
+package com.delivery.sopo.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.delivery.sopo.R
+
+class TestActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+        val fm = supportFragmentManager
+        val transaction = fm.beginTransaction()
+        transaction.run {
+            replace(R.id.layout_test_fragment, ParcelDetailView(), "Tag")
+            addToBackStack(null)
+            commit()
+        }
+    }
+}
