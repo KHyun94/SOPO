e3ebcb038d836b66fc39fe99769a7ba16c02b7aa
KHyun94 <asle1221@naver.com>
2021-04-18 17:56:33 +0900

    [UPDATE] update

--- app/src/main/java/com/delivery/sopo/views/menus/SignOutView.kt
+++ app/src/main/java/com/delivery/sopo/views/menus/SignOutView.kt
@@ -6,14 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.delivery.sopo.R
-import com.delivery.sopo.databinding.FragmentAccountManagerBinding
-import kotlinx.android.synthetic.main.fragment_account_manager.*
 
-
-class AccountManagerFragment: Fragment()
+class SignOutView: Fragment()
 {
-    lateinit var binding: FragmentAccountManagerBinding
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -22,18 +17,7 @@ class AccountManagerFragment: Fragment()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
-
-        val view = inflater.inflate(R.layout.fragment_account_manager, container, false)
-
-        layout_sign_out.setOnClickListener {
-
-        }
-
-        return view
-    }
-
-    companion object
-    {
-
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.sign_out_view, container, false)
     }
 }
